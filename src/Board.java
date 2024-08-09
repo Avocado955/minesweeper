2c65acc0d7fba903908b3efc5ffdbd9a9b2d2761
Avocado955 <mellis955@gmail.com>
2024-08-09 20:38:05 +1000

    moved the loop logic for getting and checking all the safe spaces around the given x and y into its own, uses it for reveal and updating the numbers surrounding a bomb

--- src/Board.java
+++ src/Board.java
@@ -28,6 +28,7 @@ public class Board {
   private boolean[][] revealed; // Using this to check if a board square has been revealed
   private int numOfMines;
 
+  // -----Constructors-----
   public Board() {
     this.xySize = 10;
     this.numOfMines = 10;
@@ -51,13 +52,7 @@ public class Board {
     this.revealed = new boolean[num][num];
   }
 
-  // _____________
-  // [ ] | | |
-  // [ ] | |
-  // [ ][ ][ ][ ]
-  // [ ][ ][ ][ ]
-  // ￣ ￣ ￣ ￣ ￣ ￣ ￣
-  // ---------- (U+FFE3)
+  // -----Private Methods-----
 
   private String getSpaceValue(int x, int y) {
     switch (this.board[x][y]) {
@@ -74,6 +69,76 @@ public class Board {
     String letter = Character.toString(65 + x);
     return letter;
   }
+
+  private List<GridPos> getAllSafeGridPosAroundGridPos(int x, int y) {
+    List<GridPos> allGridPositions = new ArrayList<>();
+    int a = 0;
+    int b = -1;
+    for (int z = 0; z < 8; z++) {
+      // System.out.printf("X: %d, A: %d, Y: %d, B: %d\n", x, a, y, b);
+      if (z == 1) {
+        b += 1;
+      }
+      if (z == 2) {
+        a = -1;
+        b = -1;
+      }
+      if (z == 5) {
+        a = 1;
+        b = -1;
+      }
+      GridPos newPos = new GridPos(x + a, y + b);
+      allGridPositions.add(newPos);
+      b += 1;
+    }
+    // System.out.println("All values");
+    // System.out.println(allGridPositions.toString());
+    // System.out.println();
+
+    List<GridPos> validGridPositionsToCheck = new ArrayList<>();
+    for (GridPos gridPos : allGridPositions) {
+      if (gridPos.getX() < 0 || gridPos.getX() >= this.xySize || gridPos.getY() < 0 || gridPos.getY() >= this.xySize) {
+        continue;
+      }
+      validGridPositionsToCheck.add(gridPos);
+    }
+
+    // System.out.println("After remove out of range values");
+    // System.out.println(validGridPositionsToCheck.toString());
+    // System.out.println();
+    return validGridPositionsToCheck;
+  }
+
+  private void updateAllSurroundingBomb(int x, int y) {
+    // Refactor using GridPos list
+    // need to check if its in range of the board
+    // need to check that the cell is not a bomb
+    List<GridPos> validGridPositionsToCheck = getAllSafeGridPosAroundGridPos(x, y);
+
+    for (GridPos gridPos : validGridPositionsToCheck) {
+      if (checkSpaceForBomb(gridPos.getX(), gridPos.getY())) {
+        continue;
+      }
+      this.board[gridPos.getX()][gridPos.getY()] += 1;
+    }
+
+  }
+
+  private void reveal(int x, int y) {
+    if (this.revealed[x][y]) {
+      return;
+    }
+    revealed[x][y] = true;
+    if (this.board[x][y] != 0) {
+      return;
+    }
+    List<GridPos> validGridPositionsToReveal = getAllSafeGridPosAroundGridPos(x, y);
+    for (GridPos gridPos : validGridPositionsToReveal) {
+      reveal(gridPos.getX(), gridPos.getY());
+    }
+  }
+
+  // -----Public Methods-----
 
   public void printBoard() {
     System.out.printf(" X/Y");
@@ -106,105 +171,6 @@ public class Board {
     return false;
   }
 
-  private void updateAllSurroundingBomb(int x, int y) {
-    // Refactor using GridPos list
-    List<GridPos> allGridPositions = new ArrayList<>();
-    int a = 0;
-    int b = -1;
-    for (int z = 0; z < 8; z++) {
-      System.out.printf("X: %d, A: %d, Y: %d, B: %d\n", x, a, y, b);
-      if (z == 1) {
-        b += 1;
-      }
-      if (z == 2) {
-        a = -1;
-        b = -1;
-      }
-      if (z == 5) {
-        a = 1;
-        b = -1;
-      }
-      GridPos newPos = new GridPos(x + a, y + b);
-      allGridPositions.add(newPos);
-      b += 1;
-    }
-    System.out.println("All values");
-    System.out.println(allGridPositions.toString());
-    System.out.println();
-    // Creates an error
-    // Need to either use the iterator method
-    // or make a new list and add them to it
-    List<GridPos> validGridPositionsToCheck = new ArrayList<>();
-    for (GridPos gridPos : allGridPositions) {
-      if (gridPos.getX() < 0 || gridPos.getX() >= this.xySize || gridPos.getY() < 0 || gridPos.getY() >= this.xySize) {
-        continue;
-      }
-      validGridPositionsToCheck.add(gridPos);
-    }
-
-    System.out.println("After remove out of range values");
-    System.out.println(validGridPositionsToCheck.toString());
-    System.out.println();
-
-    for (GridPos gridPos : validGridPositionsToCheck) {
-      if (checkSpaceForBomb(gridPos.getX(), gridPos.getY())) {
-        continue;
-      }
-      this.board[gridPos.getX()][gridPos.getY()] += 1;
-    }
-
-    // need to check that the cell is not a bomb
-    // need to check if its in range of the board
-
-    // For now write an if statement for each, and think about a way to streamline
-    // it
-    // if (y - 1 >= 0) {
-    // if (!checkSpaceForBomb(x, y - 1)) {
-    // this.board[x][y - 1] += 1; // Left of Bomb
-    // }
-    // if (x - 1 >= 0) {
-    // if (!checkSpaceForBomb(x - 1, y - 1)) {
-    // this.board[x - 1][y - 1] += 1; // Diagonal Left Above of Bomb
-    // }
-    // }
-    // if (x + 1 < this.xySize) {
-    // if (!checkSpaceForBomb(x + 1, y - 1)) {
-    // this.board[x + 1][y - 1] += 1; // Diagonal Left Below of Bomb
-    // }
-    // }
-    // }
-
-    // if (y + 1 < this.xySize) {
-    // if (!checkSpaceForBomb(x, y + 1)) {
-    // this.board[x][y + 1] += 1; // Right of Bomb
-    // }
-    // if (x - 1 >= 0) {
-    // if (!checkSpaceForBomb(x - 1, y + 1)) {
-    // this.board[x - 1][y + 1] += 1; // Diagonal Right Above of Bomb
-    // }
-
-    // }
-    // if (x + 1 < this.xySize) {
-    // if (!checkSpaceForBomb(x + 1, y + 1)) {
-    // this.board[x + 1][y + 1] += 1; // Diagonal Right Below of Bomb
-    // }
-    // }
-    // }
-
-    // if (x - 1 >= 0) {
-    // if (!checkSpaceForBomb(x - 1, y)) {
-    // this.board[x - 1][y] += 1; // Above Bomb
-    // }
-    // }
-
-    // if (x + 1 < this.xySize) {
-    // if (!checkSpaceForBomb(x + 1, y)) {
-    // this.board[x + 1][y] += 1; // Below Bomb
-    // }
-    // }
-
-  }
-
   public boolean checkInput(int x, int y) {
     boolean bombCheck = checkSpaceForBomb(x, y);
     if (bombCheck) {
@@ -213,51 +179,6 @@ public class Board {
     }
     reveal(x, y);
     return false;
-  }
-
-  // Have a system for reveal, have it check if space is already revealed
-  // Have it check if it is a bomb -- This should be done elsewhere before
-  // checking reveal.
-  // Then reveal this space
-  // if this space is not 0, return
-  // if it is 0 meaning its a blank space, call reveal on each of the 8
-  // surrounding
-
-  public void reveal(int x, int y) {
-    if (this.revealed[x][y]) {
-      return;
-    }
-    revealed[x][y] = true;
-    if (this.board[x][y] != 0) {
-      return;
-    }
-    if (y - 1 >= 0) {
-      reveal(x, y - 1); // Left of Bomb
-      if (x - 1 >= 0) {
-        reveal(x - 1, y - 1); // Diagonal Left Above of Bomb
-      }
-      if (x + 1 < this.xySize) {
-        reveal(x + 1, y - 1); // Diagonal Left Below of Bomb
-      }
-    }
-
-    if (y + 1 < this.xySize) {
-      reveal(x, y + 1); // Right of Bomb
-      if (x - 1 >= 0) {
-        reveal(x - 1, y + 1); // Diagonal Right Above of Bomb
-      }
-      if (x + 1 < this.xySize) {
-        reveal(x + 1, y + 1); // Diagonal Right Below of Bomb
-      }
-    }
-
-    if (x - 1 >= 0) {
-      reveal(x - 1, y); // Above Bomb
-    }
-
-    if (x + 1 < this.xySize) {
-      reveal(x + 1, y); // Below Bomb
-    }
   }
 
   public boolean checkWin() {
